6cb89c5b39b773514e5bf5d25b50e8acaaa945b1
Alireza Ahmadi <alireza.ahmadi@infrastructureontario.ca>
2018-02-17 12:41:53 -0500

    Made Git more interactive

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi, My name is Git. what is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you {name}");
+
+            Console.WriteLine($"{name} do you know how git works?");
+            var answer = Console.ReadLine();
+            if(answer == "yes"){
+                Console.WriteLine("greate! this session is over");
+                return;
+            }
+            Console.WriteLine("Dont worry. you will learn soon.");
+
+            Console.WriteLine("Lesson 1: How to create git repository");
+            Console.WriteLine(">git init");
         }
     }
 }
